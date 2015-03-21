6392499ed75ab6b762861829e4a54fe022fa8f2c
tokyorefrain <dojce1048@gmail.com>
2015-03-21 22:21:09 +0900

    基本機能が完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,5 @@
 class PostsController < ApplicationController
 
-  http_basic_authenticate_with name: "user", password: "secret", except: [:index, :show]
-
   def index
     @posts = Post.all
   end
