35927afaafb480ad4645e6ae2b520351ddc24e4c
EC2 Default User <ec2-user@ip-172-31-13-192.ap-northeast-1.compute.internal>
2015-03-22 12:31:54 +0000

    aws上でproductionモードで動作させるため微調整をした

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost:80' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.assets.compile = true
+
 end
