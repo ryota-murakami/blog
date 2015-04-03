f7d7835a22dc1959b9be7fb972706af98814ae13
tokyorefrain <dojce1048@gmail.com>
2015-04-03 22:53:58 +0900

    pick_datetimeヘルパーの引数名をtimeへ変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
       str.gsub(/<(script.?)>/x,"&lt;\\1&gt;").gsub(/<(style.?)>/x,"&lt;\\1&gt;").gsub(/\r\n|\r|\n/, "<br>").html_safe
   end
 
-  def pick_datetime(str)
-    str.to_s.gsub(/^(\d{4}-\d{2}-\d{2}) \d{2}:\d{2}:\d{2} .{3}$/,"\\1")
+  def pick_datetime(time)
+    time.to_s.gsub(/^(\d{4}-\d{2}-\d{2}) \d{2}:\d{2}:\d{2} .{3}$/,"\\1")
   end
 
 end
