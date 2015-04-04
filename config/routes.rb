ffc6af914f17fcf6507432f9f819726b9fed77a4
tokyorefrain <dojce1048@gmail.com>
2015-04-04 17:37:15 +0900

    deviseのルーティングをログイン、ログアウトに限定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'page/about'
+  devise_for :users, only: [:sign_in, :sign_out, :session]
 
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,6 +10,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'page/about' => 'page#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
