2eedc55481884cf22be5a5f96dd9b8dad18a8d58
tokyorefrain <dojce1048@gmail.com>
2015-07-17 21:40:44 +0900

    categoryモデルを定義

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319133647) do
+ActiveRecord::Schema.define(version: 20150717045124) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
     t.string   "pass"
     t.string   "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
