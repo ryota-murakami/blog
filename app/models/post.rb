67a042e5e48adfda03451a7b9176f6ad1e3aaf95
tokyorefrain <dojce1048@gmail.com>
2015-07-19 11:05:06 +0900

    pageControllerのテストを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :categories, dependent: :destroy
 
   validates :title, presence: true, length:{minimum:5}
 end
