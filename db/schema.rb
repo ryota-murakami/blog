d473cddec63c0ebe9a03605f8432565fbdf5ca42
tokyorefrain <dojce1048@gmail.com>
2015-07-19 19:20:37 +0900

    categoriesテーブルにpostsテーブルの外部キーを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717045124) do
+ActiveRecord::Schema.define(version: 20150719095843) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150717045124) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "posts_id"
   end
 
   create_table "comments", force: :cascade do |t|
