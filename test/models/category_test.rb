c68d7c72270201bc78989f5e86eb630be7b11375
ryota-murakami <dojce1048@gmail.com>
2015-07-26 14:02:02 +0900

    カテゴリ更新/削除のテストを追加

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "categoy update and delete" do
+     @post = Post.find_by_title('MyPost1')
+
+     # fixtureで自動に割り当てられたカテゴリのPRIMARY KEYを取得
+     cat_id_1, cat_id_2 = @post.categories[0]['id'], @post.categories[1]['id']
+
+     #カテゴリ1の名前を更新
+     @post.update(categories_attributes: {id: cat_id_1, name: 'updated'})
+
+     #カテゴリ1の名前が更新されているか?
+     assert @post.categories.find(cat_id_1).name == 'updated'
+
+     #カテゴリ2を削除
+     @post.update(categories_attributes: {id: cat_id_2, _destroy: 1})
+
+     #カテゴリ2が削除されているか?
+     assert_not @post.categories.exists?(id: cat_id_2)
+   end
 end
