f347ce55213d36d38f06fd7357242a89e3161523
ryota-murakami <dojce1048@gmail.com>
2015-08-08 13:43:16 +0900

    include Devise::TestHelpers

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-   test "get rss feed" do
-     get :index, format: :rss
-     assert_response :success
-   end
+  include Devise::TestHelpers
+
+  test "get rss feed" do
+    get :index, format: :rss
+    assert_response :success
+  end
+
+  test "GET #index page" do
+    get :index
+    assert_response :success
+  end
 end
