9241ec0033444adc17713dd2b6fee1a67c62029b
ryota-murakami <dojce1048@gmail.com>
2015-08-19 23:56:10 +0900

    [test]featuresスペックの記法を変更 feature/context/scenarioを採用

--- test/features/user_integrate_test.rb
+++ test/features/user_integrate_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class UserIntegrateTest < Capybara::Rails::TestCase
-  test "show index page" do
-    visit root_path
-    assert_content page, "ふつうのブログ"
+  feature "IndexPage" do
+    context "common user" do
+      scenario "show index page" do
+        visit root_path
+        assert_content page, "ふつうのブログ"
+      end
+    end
   end
 end
