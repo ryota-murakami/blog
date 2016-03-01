ecb6f90ba87a4f39436545cc31e7886094f97bbc
ryota-murakami <dojce1048@gmail.com>
2016-03-01 23:38:34 +0900

    バックアップしたDBを開発環境にも反映

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -2,6 +2,7 @@
 BLOG_DIR=/home/ec2-user/blog
 PROD_DB_FILE=$BLOG_DIR/db/production.sqlite3
 LOCAL_ROOT_DIR=$(cd $(dirname $0)/..; pwd)
-BACKUP_DEST=$LOCAL_ROOT_DIR/db/
+DB_DIR=$LOCAL_ROOT_DIR/db
 
-scp malloc.tokyo:$PROD_DB_FILE $BACKUP_DEST
+scp malloc.tokyo:$PROD_DB_FILE $DB_DIR
+cp $DB_DIR/production.sqlite3 $DB_DIR/development.sqlite3
