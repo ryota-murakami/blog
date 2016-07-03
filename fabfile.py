ce84e2c9eb35fe5e421dd7a45accb3ead00043e9
ryota-murakami <dojce1048@gmail.com>
2016-07-04 00:26:35 +0900

    rails up

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from fabric.api import cd, env, local, run, task, sudo
+from fabric.api import cd, env, local, run, task, sudo, shell_env
 from fabric.contrib.project import rsync_project
 
 env.hosts = ('malloc.tokyo')
@@ -12,10 +12,19 @@ env.remote_project_dir = ('/blog')
 def deploy():
     _rsync()
     _bundle_install()
+    _rails_up()
 
 def _bundle_install():
     with cd(env.remote_project_dir):
         run('bundle install --without test development')
+
+def _rails_up():
+    with cd(env.remote_project_dir):
+        res = run('bundle exec rake secret', warn_only=True)
+        with shell_env(SECRET_KEY_BASE=res):
+            run('bundle exec rake assets:precompile RAILS_ENV=production')
+            run('bundle exec rake db:migrate RAILS_ENV=production')
+            run('unicorn_rails -c config/unicorn.conf -E production -D')
 
 def _rsync():
     rsync_project(
