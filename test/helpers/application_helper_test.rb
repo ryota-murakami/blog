8894482f1d56fc19652b8de6e3bc9ae6e232576b
ryota-murakami <dojce1048@gmail.com>
2016-07-30 14:23:58 +0900

    hatena card https

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -10,11 +10,11 @@ describe MarkdownHelper do
 
   describe "URLをはてなブログカードに変換する機能の動作確認" do
     it "http URLがはてなブログカードに変換される" do
-      assert_match '<iframe src="http://hatenablog.com/embed?url=http://example.com" scrolling="no" frameborder="0" style="width:100%; height:155px; max-width:500px;"></iframe>', @helper.url_to_hatena_blog_card('[http://example.com:embed:cite]')
+      assert_match '<iframe src="https://hatenablog-parts.com/embed?url=http://example.com" scrolling="no" frameborder="0" style="width:100%; height:155px; max-width:500px;"></iframe>', @helper.url_to_hatena_blog_card('[http://example.com:embed:cite]')
     end
 
     it "https URLがはてなブログカードに変換される" do
-      assert_match '<iframe src="http://hatenablog.com/embed?url=https://example.com" scrolling="no" frameborder="0" style="width:100%; height:155px; max-width:500px;"></iframe>', @helper.url_to_hatena_blog_card('[https://example.com:embed:cite]')
+      assert_match '<iframe src="https://hatenablog-parts.com/embed?url=https://example.com" scrolling="no" frameborder="0" style="width:100%; height:155px; max-width:500px;"></iframe>', @helper.url_to_hatena_blog_card('[https://example.com:embed:cite]')
     end
 
     it ":embed:cite指定以外のURLは変換されない" do
