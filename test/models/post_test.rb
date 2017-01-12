1da0dab098cf099bba8807a5e459ef7b4f669ee6
ryota-murakami <dojce1048@gmail.com>
2017-01-12 23:14:14 +0900

    implement post_test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = Post.new(title: 'five5')
+  end
+
+  test 'should be valid' do
+     assert @post.valid?
+  end
+
+  test 'should be present' do
+    @post.title = nil
+
+    assert_not @post.valid?
+  end
+
+  test 'should be 5 minimum length' do
+    @post.title = 'four'
+
+    assert_not @post.valid?
+  end
 end
