cf1b972618acdd1121dc699b7a81120f4e64af63
ryota-murakami <dojce1048@gmail.com>
2017-01-12 21:58:31 +0900

    fix remove comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
-  has_many :comments, dependent: :destroy
   has_many :categories, dependent: :destroy
   accepts_nested_attributes_for :categories, allow_destroy: true
 
