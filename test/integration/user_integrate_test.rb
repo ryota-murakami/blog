88bdcef343a699a9ed761b78efb9af654d7db777
ryota-murakami <dojce1048@gmail.com>
2017-01-13 22:53:22 +0900

    [user_integrate_test.rb] fix todo

--- test/integration/user_integrate_test.rb
+++ test/integration/user_integrate_test.rb
@@ -1,22 +1,18 @@
 require 'test_helper'
 
 class UserIntegrateTest < Capybara::Rails::TestCase
+  before do
+    visit new_user_session_path
+
+    fill_in 'user[email]', with: 'test@example.org'
+    fill_in 'user[password]', with: '123greetings'
+
+    click_on 'Log in'
+  end
+
   feature 'device' do
     context 'common user' do
       scenario 'login' do
-        visit new_user_session_path
-        assert_content 'Log in'
-
-        fill_in 'user[email]', with: 'test@example.org'
-        fill_in 'user[password]', with: '123greetings'
-
-        click_on 'Log in'
-
-        assert_current_path root_path
-
-        assert_content '新しい投稿'
-
-        # TODO: ログイン処理を共通化し、別のシナリオに分割する
         click_on '新しい投稿'
 
         fill_in 'post[title]', with: 'test title'
